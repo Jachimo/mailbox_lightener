f4afc76145c0e0552381afa1fcde3792b68d9c84
Jachimo <jachimo@noreply.github.com>
2021-12-06 17:06:59 -0500

    Refactored, needs testing.

--- main.py
+++ main.py
@@ -1,39 +1,44 @@
 #!/usr/bin/env python3
 
 import sys
-import os
 import mailbox
 import email
-from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 def main(infile, outfile):
-    mbox = parse_mbox(infile)
-    outbox = mailbox.mbox(of)
+    mbox = mailbox.mbox(infile)
+    outbox = mailbox.mbox(outfile)
     for m in mbox:
         msg: email.message = email.message_from_string(m)
-        lightmessage: email.message = lighten_message(msg)
+        headers = []  # store outer headers for later use
+        for h in msg.items():
+            headers.append(h)
+        lightmessage: email.message = lighten_message(msg, headers)
         outbox.add(lightmessage)
     outbox.flush()
     outbox.close()
     return 0
 
 
-def lighten_message(msg: email.message) -> email.message:
-    if msg.is_multipart:
-        for part in msg.walk():
-            return lighten_message(part)  # descend recursively
-    else:
-        if msg.get_content_maintype().lower() == 'text/plain':
-            msg.set_payload(strip_quoteblocks(msg.get_payload()))
-            return msg
+def lighten_message(msg: email.message, headers: list) -> email.message:
+    for part in msg.walk():
+        if part.is_multipart:
+            continue
+        else:
+            if part.get_content_type().lower() == 'text/plain':
+                outmsg = MIMEText(_subtype='plain')
+                outmsg.set_payload(strip_quoteblocks(msg.get_payload()))
+                for hname, hval in headers:
+                    outmsg[hname] = hval  # copy headers from outermost message to output
+                return outmsg
 
 
-def strip_quoteblocks(lines):
+def strip_quoteblocks(lines: list) -> list:
     outlines = []
     for i in range(len(lines)):
         # look for series of 3 quoted lines together
-        if (
+        if (  # there's probably a more-elegant general way to do this for blocks of N quoted lines...
                 (is_quoted_line(lines, i) and is_quoted_line(lines, i + 1) and is_quoted_line(lines, i + 2))
                 or (is_quoted_line(lines, i) and is_quoted_line(lines, i + 1) and is_quoted_line(lines, i - 1))
                 or (is_quoted_line(lines, i) and is_quoted_line(lines, i - 1) and is_quoted_line(lines, i - 2))
@@ -44,7 +49,7 @@ def strip_quoteblocks(lines):
     return lines
 
 
-def is_quoted_line(lines, i):
+def is_quoted_line(lines: list, i: int) -> bool:
     try:
         if '>' in lines[i][:3] and not '<' in lines[i]:
             return True
@@ -52,29 +57,6 @@ def is_quoted_line(lines, i):
             return False
     except IndexError:
         return False
-
-
-def is_quoted_block(lines, blocksize, i):
-    lookahead = blocksize - 1
-    if i < (len(lines) - lookahead):
-        for j in range(lookahead):
-            if not is_quoted_line(lines, i+j):
-                return False
-            else:
-                continue
-        return True
-    else:  # last N lines of file where N = blocksize
-    lookbehind = len(lines) - (i + blocksize)  # should be negative
-    lookahead = lookahead + lookbehind
-
-
-
-def parse_mbox(infile):
-    messages = []
-    mbox = mailbox.mbox(infile)
-    for msg in mbox:
-        messages.append(msg)
-    return messages
 
 
 if __name__ == '__main__':
